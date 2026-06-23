b9ea919247085db36f3fe17a1fdc3d00b3a7c803
Denis Jajecznica <loned4@gmail.com>
2026-06-23 23:07:31 +0200

    6.2 Added Calculator class and testing main

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.Calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+
+        int addResult = calculator.add(10, 5);
+        if (addResult == 15) {
+            System.out.println("Test dodawania OK");
+        } else {
+            System.out.println("Test dodawania BŁĄD!");
+        }
+
+
+        int subResult = calculator.subtract(10, 5);
+        if (subResult == 5) {
+            System.out.println("Test odejmowania OK");
+        } else {
+            System.out.println("Test odejmowania BŁĄD!");
+        }
     }
 }
