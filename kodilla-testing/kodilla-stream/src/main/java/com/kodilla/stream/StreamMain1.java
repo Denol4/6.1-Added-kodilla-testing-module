055a6b7c6193b228febde359a85f1d05e4c62d13
Denis <loned4@gmail.com>
2026-08-02 15:39:02 +0200

    zadanie 7.2

---
+++ kodilla-testing/kodilla-stream/src/main/java/com/kodilla/stream/StreamMain1.java
@@ -0,0 +1,8 @@
+package com.kodilla.stream;
+
+public class StreamMain1 {
+    public static void main(String[] args) {
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        com.kodilla.stream.iterate.NumbersGenerator.generateEven(20);
+    }
+}
